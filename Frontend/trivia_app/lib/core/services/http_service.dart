5cfb842d3f94cd24c755c65c6f762fe088031033
Elena <estamenkovskaa@gmail.com>
2025-01-26 19:14:02 +0100

    Make questions screen according to design

--- Frontend/trivia_app/lib/core/services/http_service.dart
+++ Frontend/trivia_app/lib/core/services/http_service.dart
@@ -1,78 +1,78 @@
-import 'dart:io';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:trivia_app/core/extensions/http_response_extension.dart';
-import 'package:trivia_app/globals.dart';
+// import 'dart:io';
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
+// import 'package:trivia_app/core/extensions/http_response_extension.dart';
+// import 'package:trivia_app/globals.dart';
 
-const headers = {
-  HttpHeaders.acceptHeader: 'application/json',
-  HttpHeaders.contentTypeHeader: 'application/json'
-};
+// const headers = {
+//   HttpHeaders.acceptHeader: 'application/json',
+//   HttpHeaders.contentTypeHeader: 'application/json'
+// };
 
-class HttpService {
-  Future<dynamic> get(
-    String path, {
-    Map<String, dynamic>? queryParameters,
-  }) async {
-    final url = buildUri(path, queryParameters);
+// class HttpService {
+//   Future<dynamic> get(
+//     String path, {
+//     Map<String, dynamic>? queryParameters,
+//   }) async {
+//     final url = buildUri(path, queryParameters);
 
-    final response = await http.get(
-      url,
-      headers: headers,
-    );
-    response.ensureSuccessStatusCode();
-    return _decodeBody(response.body);
-  }
+//     final response = await http.get(
+//       url,
+//       headers: headers,
+//     );
+//     response.ensureSuccessStatusCode();
+//     return _decodeBody(response.body);
+//   }
 
-  Future<dynamic> post(String path, Object requestBody) async {
-    final endpoint = Uri.parse(baseApiUrl + path);
+//   Future<dynamic> post(String path, Object requestBody) async {
+//     final endpoint = Uri.parse(baseApiUrl + path);
 
-    final response = await http.post(
-      endpoint,
-      headers: headers,
-      body: jsonEncode(requestBody),
-    );
-    response.ensureSuccessStatusCode();
-    return _decodeBody(response.body);
-  }
+//     final response = await http.post(
+//       endpoint,
+//       headers: headers,
+//       body: jsonEncode(requestBody),
+//     );
+//     response.ensureSuccessStatusCode();
+//     return _decodeBody(response.body);
+//   }
 
-  Future<dynamic> put(String path, Object requestBody) async {
-    final endpoint = Uri.parse(baseApiUrl + path);
+//   Future<dynamic> put(String path, Object requestBody) async {
+//     final endpoint = Uri.parse(baseApiUrl + path);
 
-    final response = await http.put(
-      endpoint,
-      headers: headers,
-      body: jsonEncode(requestBody),
-    );
-    response.ensureSuccessStatusCode();
-    return _decodeBody(response.body);
-  }
+//     final response = await http.put(
+//       endpoint,
+//       headers: headers,
+//       body: jsonEncode(requestBody),
+//     );
+//     response.ensureSuccessStatusCode();
+//     return _decodeBody(response.body);
+//   }
 
-  Future<dynamic> delete(String path) async {
-    final endpoint = Uri.parse(baseApiUrl + path);
+//   Future<dynamic> delete(String path) async {
+//     final endpoint = Uri.parse(baseApiUrl + path);
 
-    final response = await http.delete(
-      endpoint,
-      headers: headers,
-    );
-    response.ensureSuccessStatusCode();
-    return _decodeBody(response.body);
-  }
-}
+//     final response = await http.delete(
+//       endpoint,
+//       headers: headers,
+//     );
+//     response.ensureSuccessStatusCode();
+//     return _decodeBody(response.body);
+//   }
+// }
 
-dynamic _decodeBody(String body) {
-  if (body.isEmpty) {
-    return {};
-  }
-  return jsonDecode(body);
-}
+// dynamic _decodeBody(String body) {
+//   if (body.isEmpty) {
+//     return {};
+//   }
+//   return jsonDecode(body);
+// }
 
-Uri buildUri(String endpoint, Map<String, dynamic>? queryParameters) {
-  final uri = Uri.parse('$baseApiUrl$endpoint');
+// Uri buildUri(String endpoint, Map<String, dynamic>? queryParameters) {
+//   final uri = Uri.parse('$baseApiUrl$endpoint');
 
-  if (queryParameters?.isNotEmpty ?? false) {
-    return uri.replace(queryParameters: queryParameters);
-  }
+//   if (queryParameters?.isNotEmpty ?? false) {
+//     return uri.replace(queryParameters: queryParameters);
+//   }
 
-  return uri;
-}
+//   return uri;
+// }
