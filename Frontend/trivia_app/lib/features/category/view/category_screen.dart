d072d96cbb9f10d04ddde9361a0b68aac491befb
teodora-pavlovska <teodora.pavlovska@students.finki.ukim.mk>
2025-01-26 14:53:33 +0100

    Update category screen

--- Frontend/trivia_app/lib/features/category/view/category_screen.dart
+++ Frontend/trivia_app/lib/features/category/view/category_screen.dart
@@ -34,11 +34,17 @@ class _CategoryScreenState extends State<CategoryScreen> {
     Random random = Random();
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home"),
-        backgroundColor: const Color.fromARGB(255, 220, 63, 144),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
         actions: [
           IconButton(
-            icon: const Icon(Icons.person),
+            icon: const Icon(Icons.person, color: Colors.black),
             onPressed: () {
               Navigator.push(
                 context,
