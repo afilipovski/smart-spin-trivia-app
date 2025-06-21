5a60f7493fcb671b595f9a90182c00d1e4c748b8
elenastamenkovska <elena.stamenkovska@codechem.com>
2025-06-21 17:39:27 +0200

    Add load spinner

--- Frontend/trivia_app/lib/features/user_profile/view/user_profile_screen.dart
+++ Frontend/trivia_app/lib/features/user_profile/view/user_profile_screen.dart
@@ -9,11 +9,7 @@ import 'package:trivia_app/features/friends/view/add_friend_screen.dart';
 import 'package:trivia_app/features/quiz/view/quiz_screen.dart';
 
 class ProfileScreen extends StatefulWidget {
-
-  const ProfileScreen({
-    super.key,
-    
-  });
+  const ProfileScreen({super.key});
 
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
@@ -27,12 +23,22 @@ class _ProfileScreenState extends State<ProfileScreen> {
   List<UserFriendship>? userFriendships;
   StreakDto? streakDto;
 
+  bool _isLoading = true;
+
   @override
   void initState() {
     super.initState();
-    _loadUserProfile();
-    _loadUserFriendships();
-    _getStreak();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    setState(() => _isLoading = true);
+    await Future.wait([
+      _loadUserProfile(),
+      _loadUserFriendships(),
+      _getStreak(),
+    ]);
+    setState(() => _isLoading = false);
   }
 
   Future<void> _loadUserProfile() async {
@@ -67,171 +73,164 @@ class _ProfileScreenState extends State<ProfileScreen> {
           onPressed: () {
             Navigator.pushReplacement(
               context,
-              MaterialPageRoute(
-                builder: (context) => QuizScreen(),
-              ),
+              MaterialPageRoute(builder: (context) => QuizScreen()),
             );
           },
         ),
       ),
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
+        child: _isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : Padding(
+                padding: const EdgeInsets.all(20.0),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    CircleAvatar(
-                      radius: 50,
-                      backgroundColor: Colors.grey[300],
-                      child: const Icon(
-                        Icons.person,
-                        size: 50,
-                        color: Colors.black,
+                    Center(
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            radius: 50,
+                            backgroundColor: Colors.grey[300],
+                            child: const Icon(
+                              Icons.person,
+                              size: 50,
+                              color: Colors.black,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            userProfile?.fullName ?? '',
+                            style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            userProfile?.email ?? '',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Divider(),
+                    Text(
+                      "Personal Information",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[700],
                       ),
                     ),
                     const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        const Icon(Icons.calendar_today, size: 16),
+                        const SizedBox(width: 17),
+                        Text(
+                          "Date of Birth: ${userProfile?.birthDate.day}.${userProfile?.birthDate.month}.${userProfile?.birthDate.year}",
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        const Icon(Icons.local_fire_department, color: Colors.orange),
+                        const SizedBox(width: 10),
+                        Text(
+                          "Daily Streak: ${userProfile?.streak} Days",
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    const Divider(),
                     Text(
-                      userProfile?.fullName ?? '',
-                      style: const TextStyle(
-                        fontSize: 24,
+                      "Friends and their streaks",
+                      style: TextStyle(
+                        fontSize: 16,
                         fontWeight: FontWeight.bold,
+                        color: Colors.grey[700],
                       ),
                     ),
-                    Text(
-                      userProfile?.email ?? '',
-                      style: const TextStyle(
-                        fontSize: 16,
-                        color: Colors.grey,
-                      ),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: userFriendships != null && userFriendships!.isNotEmpty
+                          ? ListView.builder(
+                              itemCount: userFriendships!.length,
+                              itemBuilder: (context, index) {
+                                final friendship = userFriendships![index];
+                                if (!friendship.friendshipAccepted) return const SizedBox.shrink();
+
+                                var friend = friendship.friendshipReceiver;
+                                if (friend != null && friend.id == userProfile?.id) {
+                                  friend = friendship.friendshipInitiator;
+                                }
+
+                                return ListTile(
+                                  leading: CircleAvatar(
+                                    backgroundColor: Colors.grey[300],
+                                    child: Text(
+                                      friend!.fullName[0].toUpperCase(),
+                                      style: const TextStyle(color: Colors.black),
+                                    ),
+                                  ),
+                                  title: Text(friend.fullName),
+                                  trailing: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      const Icon(Icons.local_fire_department, color: Colors.orange),
+                                      const SizedBox(width: 5),
+                                      Text("${friend.streak}"),
+                                    ],
+                                  ),
+                                );
+                              },
+                            )
+                          : const Center(
+                              child: Text("No friends yet."),
+                            ),
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(builder: (context) => const AddFriendScreen()),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF8668FF),
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 16.0,
+                                horizontal: 40.0,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30.0),
+                              ),
+                            ),
+                            child: const Text(
+                              'Add Friends',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
               ),
-              const SizedBox(height: 20),
-              const Divider(),
-              Text(
-                "Personal Information",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey[700],
-                ),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  const Icon(Icons.calendar_today, size: 16),
-                  const SizedBox(width: 17),
-                  Text(
-                    "Date of Birth: ${userProfile?.birthDate.day}.${userProfile?.birthDate.month}.${userProfile?.birthDate.year}",
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  const Icon(Icons.local_fire_department, color: Colors.orange),
-                  const SizedBox(width: 10),
-                  Text("Daily Streak: ${userProfile?.streak} Days",
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              const Divider(),
-              Text(
-                "Friends and their streaks",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey[700],
-                ),
-              ),
-              const SizedBox(height: 10),
-              Expanded(
-                child: userProfile != null &&
-                        userFriendships != null &&
-                        userFriendships!.isNotEmpty
-                    ? ListView.builder(
-                        itemCount: userFriendships!.length,
-                        itemBuilder: (context, index) {
-                          if(!userFriendships![index].friendshipAccepted){
-                            return null;
-                          }
-                          var friend = userFriendships![index].friendshipReceiver;
-                          if(friend != null && userProfile!.id == friend.id){
-                            friend = userFriendships![index].friendshipInitiator;
-                          }
-                          return ListTile(
-                            leading: CircleAvatar(
-                              backgroundColor: Colors.grey[300],
-                              child: Text(
-                                friend!.fullName[0].toUpperCase(),
-                                style: const TextStyle(color: Colors.black),
-                              ),
-                            ),
-                            title: Text(friend.fullName),
-                            trailing: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                const Icon(Icons.local_fire_department,
-                                    color: Colors.orange),
-                                const SizedBox(width: 5),
-                                Text("${friend.streak}"),
-                              ],
-                            ),
-                          );
-                        },
-                      )
-                    : const Center(
-                        child: Text("No friends yet."),
-                      ),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pushReplacement(
-                          MaterialPageRoute(
-                            builder: (context) => const AddFriendScreen(),
-                          ),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF8668FF),
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 16.0,
-                          horizontal: 40.0,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                        ),
-                      ),
-                      child: const Text(
-                        'Add Friends',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
       ),
     );
   }
